99930242c85796bf10919b6894423ff61ac09b38
hamzaarif431 <hamzaarif451997@gmail.com>
2021-09-29 10:53:46 -0700

    Poseidion-consulting

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<!--footer start-->
+   <footer>          
+    <div class="bottom-footer-clr">
+	  <div class="end-bar container cdr">
+		  <div class="row header-left">
+			  <div class="font-awesome-align">
+				  <div class="inner-font"><a class="linkfooter font-border" href=""><i class="fab fa-facebook-f"></i></a></div>
+                  <div class="inner-font"><a class="linkfooter font-border" href=""><i class="fab fa-twitter"></i></a></div>
+                <div class="inner-font"><a class="linkfooter font-border" href=""><i class="fab fa-google-plus-g"></i></a></div>
+                <div class="inner-font"><a class="linkfooter font-border" href=""><i class="fab fa-linkedin-in"></i></a></div>
+                  <div class="inner-font"><a class="linkfooter font-border" href=""><i class="fab fa-instagram"></i></a></div>
+			  </div>
+			  <div class=" overlapl">
+				  <p class="ft2 linkfooter">All Rights Reserved 2020</p>
+			  </div>
+		  </div>
+	  </div>
+	</div>
+ </footer>
+<!--footer close-->
+
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script src="js/bootstrap.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="js/wow.min.js">  </script>
+      <script type="text/javascript" src="slick/slick.min.js"></script> 
+      <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script> 
+      <script type="text/javascript">
+         new WOW().init();
+      </script>
+   </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title> Home  </title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Free Web tutorials">
+      <meta name="keywords" content="HTML, CSS, JavaScript">
+      <meta name="author" content="John Doe">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+      <link href="css/custom.css" rel="stylesheet" type="text/css">
+      <link href="slick/slick-theme.css" rel="stylesheet" type="text/css">
+      <link href="css/animate.css" rel="stylesheet" type="text/css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+      <link rel="icon" href="images/fav-icon.png" type="image/png" sizes="">
+   </head>
+   <body>
+      <header>
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+               <a class="navbar-brand" href="#">Navbar</a>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                     <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                     </li>
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                           <li><a class="dropdown-item" href="#">Action</a></li>
+                           <li><a class="dropdown-item" href="#">Another action</a></li>
+                           <li>
+                              <hr class="dropdown-divider">
+                           </li>
+                           <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                     </li>
+                  </ul>
+                  <form class="d-flex">
+                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                     <button class="btn btn-outline-success" type="submit">Search</button>
+                  </form>
+               </div>
+            </div>
+         </nav>
+      </header>
